d14ce680492c1523e7f5f967846440e240b982ac
Manu Goudar <manugv@sron.nl>
2023-08-22 06:21:58 +0200

    Added function to convert dict to class object

--- L4/ModuleDataContainer.py
+++ L4/ModuleDataContainer.py
@@ -9,3 +9,16 @@ Created on Fri Sep 23 12:54:22 2022
 
 class DataCont:
     pass
+
+
+
+class Dicttoclass:
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            if isinstance(value, dict):
+                self.__dict__[key] = Struct(**value)
+            else:
+                self.__dict__[key] = value
+
+
+    

Bug fix in units: km to mts

teds/lib/libATM.py
@@ -42,6 +42,7 @@ def shrink_extend_domain(gm_x, gm_y, x, y):
     
     """
     # get bounds of the GM domain
+    breakpoint()
     _x = np.array([gm_x[0,0], gm_x[0,-1], gm_x[-1,0], gm_x[-1,-1]])
     _y = np.array([gm_y[0,0], gm_y[0,-1], gm_y[-1,0], gm_y[-1,-1]])
 
@@ -118,17 +119,15 @@ def get_atmosphericdata_new(gm_lat, gm_lon, meteo_settings):
     # shrink or extend domain according to the input
 
     src = data.__getattribute__(meteo_settings['gases'][0]+"_source")
-
     # # if the mesh is regular (see Topology section in https://www.xdmf.org/index.php/XDMF_Model_and_Format)
     # if data.gridtype == "3DCoRectMesh":
     # create a transform method 
     trans = TransformCoords(src[1:])
     # convert lat-lon of gm to x-y and get bounds
-    gm_x, gm_y = trans.latlon2xykm(gm_lat, gm_lon)
-    x_new, y_new, idx, idy, padx, pady = shrink_extend_domain(gm_x, gm_y, data.lat, data.lon)
+    gm_x, gm_y = trans.latlon2xymts(gm_lat, gm_lon)
+    x_new, y_new, idx, idy, padx, pady = shrink_extend_domain(gm_x, gm_y, data.x, data.y)
     XX, YY = np.meshgrid(x_new, y_new)
-    lat_new, lon_new = trans.xykm2latlon(XX, YY)
-
+    lat_new, lon_new = trans.xymts2latlon(XX, YY)
     # else:
     #     print("the grids are not regular")
     #     exit()
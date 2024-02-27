bug in gain calculation fixed (libINV).

teds/L1L2/l1bl2.py
@@ -170,6 +170,7 @@ def level2_diags_output(filename, l2product, measurement):
     nact = len(l2product[0])
     nwave = len(measurement[0, 0]['wavelength'])
     # file name
+    print('diag_output')
     output_l2diag = nc.Dataset(filename, mode='w')
     output_l2diag.title = 'Tango Carbon E2ES L2 diagnostics'
     output_l2diag.createDimension('bins_spectral', nwave)     # spectral axis
@@ -201,15 +202,13 @@ def level2_diags_output(filename, l2product, measurement):
     _ = writevariablefromname(output_l2diag, "l2solarirradiance", _dims, l2diag_sun_irradiance)
     # gain CO2
     _ = writevariablefromname(output_l2diag, "l2gainCO2", _dims, l2diag_gainCO2)
-    # gain CO2
-    _ = writevariablefromname(output_l2diag, "l2gainCO2", _dims, l2diag_gainCO2)
     # gain CH4
     _ = writevariablefromname(output_l2diag, "l2gainCH4", _dims, l2diag_gainCH4)
     # gain H2O
     _ = writevariablefromname(output_l2diag, "l2gainH2O", _dims, l2diag_gainH2O)
     output_l2diag.close()
 
-def level1b_to_level2_processor(config):
+def level1b_to_level2_processor(config, sw_diag_output = False):
     # get the l1b files
     l1b = get_l1b(config['l1b_input'])
 
@@ -413,10 +412,6 @@ def level1b_to_level2_processor(config):
                 l2product[ialt_l2, iact]['XCO2 proxy precision'] = float("nan")
                 l2product[ialt_l2, iact]['XCH4 proxy precision'] = float("nan")
 
-            if(iact == 10):
-                print(l2product[0, 10]['XCO2 proxy']*1.E6)
-                print(l2product[0, 10]['XCO2 proxy precision']*1.E6)
-
             # XCO2[iact] = l2product[ialt_l2, iact]['XCO2 proxy']*1.E6
             # XCO2_prec[iact] = l2product[ialt_l2, iact]['XCO2 proxy precision']*1.E6
             # XCO2_true_smoothed[iact] = np.dot(l2product[ialt_l2, iact]['XCO2 col avg kernel'],
@@ -425,12 +420,14 @@ def level1b_to_level2_processor(config):
 
     # output to netcdf file
     level2_output(config['l2_output'], l2product, retrieval_init, l1b, config['retrieval_init'])
-    # have to be fixed because of variable spectral size of measurement vector using masked arrays
-#    level2_diags_output(config['l2_diags'], l2product, measurement)
+    
+    if(sw_diag_output):
+        level2_diags_output(config['l2_diag'], l2product, measurement)
 
     print('=> l1bl2 finished successfully')
 
-#    print(runtime_cum)
+#    print('cumulative run time: ',runtime_cum)
+
     return
 
 def level2_nan(retrieval_init, nlay):

teds/SGM/sgm.py
@@ -207,6 +207,12 @@ def sgm_output_atm_raw(filename_atm, meteo, atm_std, gases=None):
     
     # albedo
     _ = writevariablefromname(output_atm, 'albedo', _dims, albedo)
+
+    xdistance = meteo.x_new
+    ydistance = meteo.y_new
+    _ = writevariablefromname(output_atm, 'xdistance', 'bins_across_track', xdistance)
+    _ = writevariablefromname(output_atm, 'ydistance', 'bins_along_track', ydistance)
+
     # column_co2
     var_co2 = writevariablefromname(output_atm, 'XCO2', _dims, xco2)
     # write new attributes
@@ -282,7 +288,7 @@ def scene_generation_module(config):
     atm_std = libATM.get_AFGL_atm_homogenous_distribution(config['afgl_input'], nlay, dzlay)
 
     if ((config['profile'] == 'individual_spectra') or (config['profile'] == 'single_swath')):
-        # xco2 = np.sum(atm_std.CO2) / np.sum(atm_std.air) * 1.E6
+        xco2 = np.sum(atm_std.CO2) / np.sum(atm_std.air) * 1.E6
         atm = np.ndarray((nalt, nact), np.object_)
         for ialt in range(nalt):
             for iact in range(nact):
@@ -506,7 +512,7 @@ def convolvedata(meteodata, config):
     return meteodata
 
 
-def scene_generation_module_new(config):
+def scene_generation_module_new(config, sw_raw_geo_data_only=False):
     """Scene generation module.
 
     Parameters
@@ -545,11 +551,6 @@ def scene_generation_module_new(config):
         else:     
             # meteorological data
             
-            pickle.dump(gm_data, open('./test.pkl', 'wb'))
-            print(config['meteo'])
-            breakpoint()
-            
-            sys.exit()
             meteodata = libATM.get_atmosphericdata_new(gm_data['lat'], gm_data['lon'], config['meteo'])
             # get albedo on the microhh grid
 
@@ -565,13 +566,23 @@ def scene_generation_module_new(config):
             if(config['sw_geo_output_raw']):
                 sgm_output_atm_raw(config['geo_output_raw'], meteodata, atm_std, config["meteo"]['gases'])
 
+            #this is a temporary solution and requires later a full seperation
+            #of geo-calculation and RTM
+            
+            if(sw_raw_geo_data_only):
+                if __name__ == '__main__':
+                    sys.exit()
+                else:
+                    return
+
             # convolution meteo data
             meteodata = convolvedata(meteodata, config)
             # interpolation meteo data to gm grid
-            albedo, sgmmeteo = interpolate_data(meteodata, gm_data, config)
+            albedo, sgmmeteo = interpolate_data(meteodata, gm_data, config["meteo"]["gases"])
             # get collocated meteo data
             atm = libATM.combine_meteo_standard_atm(sgmmeteo, atm_std, config["meteo"]['gases'])
 
+    sys.exit('jojo')
     # =============================================================================
     # Write atmosphere and albedo data
     if (config['profile'] == 'orbit') & ~(config['only_afgl']):

teds/lib/libINV.py
@@ -200,9 +200,8 @@ def Gauss_Newton_iteration(retrieval_init, atm, optics, measurement, max_iter, c
         scaling = retrieval_init['trace gases'][key]['scaling']
         output['X'+key] = sum(atm.__getattribute__(key))/sum(atm.air)
         output['X'+key+' precision'] = output['X'+key] * x_lst_precision[l]           # ppm
-        ref_mixing_ratio = np.sum(retrieval_init['trace gases'][key]['ref_profile'])/sum(atm.air)/scaling
+        ref_mixing_ratio = x_dic[key]*np.sum(retrieval_init['trace gases'][key]['ref_profile'])/sum(atm.air)/scaling
         output['gain_X'+key] = g_dic[key] * ref_mixing_ratio
-
     m = 0
     while 'alb%d' % (m) in retrieval_init['surface'].keys():
         output['alb%d' % (m)] = x_dic["alb%d" % (m)]
@@ -235,7 +234,7 @@ def Gauss_Newton_iteration(retrieval_init, atm, optics, measurement, max_iter, c
                 delta_prof = col/retrieval_init['trace gases']['H2O']['ref_profile'][klay]
                 output['XH2O col avg kernel'][klay] = np.sum(Gain[ispec, :]*fwd['layer_'+value][:, klay])*delta_prof
 
-    # print(np.sum(output['XCO2 col avg kernel']*retrieval_init['trace gases']['CO2']['ref_profile']))
+    #    print(np.sum(output['XCO2 col avg kernel']*retrieval_init['trace gases']['CO2']['ref_profile']))
     # print(np.sum(retrieval_init['trace gases']['CO2']['ref_profile']))
     
     # plt.plot(output['XCO2 col avg kernel'], atm.zlay*1.E-3, label = 'CO2', color = 'blue')
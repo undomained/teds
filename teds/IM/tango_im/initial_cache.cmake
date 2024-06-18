c03babbc428f7a10f491502440278642f0dddad5
Raul Laasner <raullaasner@gmail.com>
2024-06-18 21:41:49 +0200

    Merge KNMI initial cache file

--- teds/IM/tango_im/initial_cache.cmake
+++ teds/IM/tango_im/initial_cache.cmake
@@ -1,29 +1,30 @@
-# Set compilers
-set(CMAKE_CXX_COMPILER /usr/lib64/ccache/g++ CACHE STRING "")
-set(CMAKE_C_COMPILER /usr/lib64/ccache/gcc CACHE STRING "")
+# GNU compilers
+set(CMAKE_CXX_COMPILER g++ CACHE STRING "")
+set(CMAKE_C_COMPILER gcc CACHE STRING "")
 
-# Set compiler flags
+# Flags
 set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG" CACHE STRING "")
 set(CMAKE_CXX_FLAGS_DEBUG "-Og -g -Wall -Wextra -Wcast-align -Wformat -Winvalid-pch -Wmissing-declarations -Wmissing-include-dirs -Wconversion -Wredundant-decls -Wswitch-default -Wswitch-enum -pedantic" CACHE STRING "")
 
 # Whether to build in Release or Debug configuration.
 set(CMAKE_BUILD_TYPE Release CACHE STRING "")
 
+# Where to install the CKD instrument model
+set(CMAKE_INSTALL_PREFIX /usr/local/ckdmodel CACHE STRING "")
+
 # Additional directories containing header files
 set(INCLUDE_PATHS /usr/include CACHE STRING "")
 
-# Linear algebra library directories
+# Library directories
 set(LIBRARY_PATHS /usr/lib64 CACHE STRING "")
 
-# Linear algebra libraries
-set(LIBRARIES libnetcdf_c++4.so libnetcdf.so liblapack.so.3.11.0 CACHE STRING "")
+# Set Libraries
+set(LIBRARIES netcdf_c++4 netcdf lapack CACHE STRING "")
 
-# Where to install
-set(CMAKE_INSTALL_PREFIX /usr/local/tango CACHE STRING "")
+# The IM additionally depends on the L1A-L1B processor
+set(TANGO_L1B_PATH ../../L1AL1B/tango_l1b/build/ CACHE STRING "")
 
-# If installed, point to where the dependencies are located. This is
-# not necessary if they are in a standard location installed by your
-# package manager.
+# Like with the L1A-L1B processor, point to where the dependencies are located.
 set(SPDLOG_PATH /usr/local/spdlog CACHE STRING "")
 set(YAML_CPP_PATH /usr/local/yaml-cpp CACHE STRING "")
 set(POCKETFFT_PATH /usr/local/pocketfft CACHE STRING "")

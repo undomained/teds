2a3a43dd200e81bd2ebef5529034c4fb5d8d9b29
Mirna van Hoek <mirna.van.hoek@knmi.nl>
2024-07-02 16:27:15 +0200

    Added code to deal with outputfile and reshape if needed for L1B step. Extra protection when dealing with IM and L1B configuration

--- teds/run_E2E.py
+++ teds/run_E2E.py
@@ -44,6 +44,8 @@ def build(logger, config, step, cfg_path, attribute_dict):
         - attribute_dict: Dictionary with attributes to be added to main of output netCDF files
     """
 
+    configuration = config.copy()
+
     if step == 'gm' or step == 'all':
 
         E2EModule = importlib.import_module("GM.gm")
@@ -72,18 +74,25 @@ def build(logger, config, step, cfg_path, attribute_dict):
         # need the info one level up in config yaml file. 
         # write out temp yaml file
 
+        im_configuration = configuration.copy()
         # Ensure we are using the IM CKD and l1b files (in IM it means the input file)
-        config['io']['l1b'] = im_input
-        config['io']['ckd'] = im_ckd
+#        config['io']['l1b'] = im_input
+#        config['io']['ckd'] = im_ckd
+        im_configuration['io']['l1b'] = im_input
+        im_configuration['io']['ckd'] = im_ckd
         # Get the IM specific steps and move them one level up
         im_config = config['instrument_model']
-        for key, value in im_config.items():
-            config[key] = value
+        for key, value in im_configuration.items():
+#            config[key] = value
+            im_configuration[key] = value
+
+        del im_configuration['instrument_model']
+        del im_configuration['l1b']
 
         # write config to temp yaml file with IM values filled in
         im_config_file = f"{cfg_path}/im_config_temp.yaml"
         with open(im_config_file,'w') as outfile:
-            yaml.dump(config, outfile)
+            yaml.dump(im_configuration, outfile)
 
         # Create cfg file to be used for IM executable
 
@@ -95,16 +104,16 @@ def build(logger, config, step, cfg_path, attribute_dict):
         # Need to call C++
         subprocess.run(["IM/tango_im/build/tango_im.x", im_config_file])
         # readin output file
-        l1a_file = config['io']['l1a']
+        l1a_file = im_configuration['io']['l1a']
         l1a_data = dn.DataNetCDF(logger, l1a_file, mode='r')
 #        if config['cal_level'] == 'swath' or config['cal_level'] == 'rad':
-        if config['cal_level'] == 'rad':
+        if im_configuration['cal_level'] == 'rad':
             print("Not detector image")
         else:
             # Get data
             data = l1a_data.get('detector_image',  group='science_data', kind = 'variable')
             # Get dimensions from ckd?
-            ckd_file = config['io']['ckd']
+            ckd_file = im_configuration['io']['ckd']
             ckd_data = dn.DataNetCDF(logger, ckd_file, mode='r')
             det_rows = ckd_data.get('detector_row', kind='dimension')
             det_cols = ckd_data.get('detector_column', kind='dimension')
@@ -114,10 +123,25 @@ def build(logger, config, step, cfg_path, attribute_dict):
             l1a_data.add(name='along_track', value=data.shape[0], kind='dimension')
     
             # reshape data
-            data_reshaped = np.reshape(data, (data.shape[0], det_rows, det_cols))
+            # How do I get the right dimensions when binning has been applied?
+            # For now stupidly devide det_rows by table_id
+            bin_file = im_configuration['io']['binning_table']
+            bin_data = dn.DataNetCDF(logger, bin_file, mode='r')
+            bin_id = im_configuration['detector']['binning_table_id']
+            table = f"Table_{bin_id}"
+            print(f"Binning table: {table}")
+            binned_pixels = bin_data.get('bins', group=table, kind='dimension')
+            binned_rows = int(binned_pixels/det_cols)
+            l1a_data.add(name='binned_row', value=binned_rows, kind='dimension')
+
+#            data_reshaped = np.reshape(data, (data.shape[0], det_rows, det_cols))
+            data_reshaped = np.reshape(data, (data.shape[0], binned_rows, det_cols))
             print(f"Reshaped data shape : {data_reshaped.shape}")
             print(f"Reshaped data image 1: {data_reshaped[1,:,:]}")
-            l1a_data.add(name='detector_image_3d', value=data_reshaped, group='science_data', dimensions=('along_track','detector_row','detector_column'), kind='variable')
+            if config['cal_level'] != 'l1a':
+                l1a_data.add(name='detector_image_3d', value=data_reshaped, group='science_data', dimensions=('along_track','detector_row','detector_column'), kind='variable')
+            else:
+                l1a_data.add(name='detector_image_3d', value=data_reshaped, group='science_data', dimensions=('along_track','binned_row','detector_column'), kind='variable')
     
             print("###################################################################")
             print("###################################################################")
@@ -127,7 +151,7 @@ def build(logger, config, step, cfg_path, attribute_dict):
             # write to temp output file
             l1a_data.write('l1a_cpp_temp.nc')
 
-        Utils.add_attributes_to_output(logger, config['io']['l1a'], attribute_dict)
+        Utils.add_attributes_to_output(logger, im_configuration['io']['l1a'], attribute_dict)
 
 
     if step == 'l1al1b' or step == 'all':
@@ -137,24 +161,87 @@ def build(logger, config, step, cfg_path, attribute_dict):
 #        # Need to call C++
 #        subprocess.run(["L1AL1B/tango_l1b/build/tango_l1b", f"{cfg_path}/l1al1b_config.cfg"])
 
+        l1b_configuration = configuration.copy()
+
         # Ensure we are using the IM CKD and l1b files (in IM it means the input file)
-        config['io']['ckd'] = l1b_ckd
-        config['io']['l1b'] = l1b_file
+        l1b_configuration['io']['ckd'] = l1b_ckd
+        l1b_configuration['io']['l1b'] = l1b_file
         # Get the L1B specific steps and move them one level up
-        im_config = config['l1b']
-        for key, value in im_config.items():
-            config[key] = value
+        l1b_config = configuration['l1b']
+        for key, value in l1b_config.items():
+            l1b_configuration[key] = value
+
+        del l1b_configuration['instrument_model']
+        del l1b_configuration['l1b']
 
         # write config to temp yaml file with IM values filled in
         l1b_config_file = f"{cfg_path}/l1b_config_temp.yaml"
         with open(l1b_config_file,'w') as outfile:
-            yaml.dump(config, outfile)
+            yaml.dump(l1b_configuration, outfile)
 
         # Need to call C++
 #        subprocess.run(["L1AL1B/tango_l1b/build/tango_l1b.x", cfgFile])
-        subprocess.run(["L1AL1B/tango_l1b/build/tango_l1b.x", l1b_config_file])
+        result = subprocess.run(["L1AL1B/tango_l1b/build/tango_l1b.x", l1b_config_file])
+#        result = subprocess.run(["L1AL1B/tango_l1b/build/tango_l1b.x", l1b_config_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+#        print("STDOUT:")
+#        print(result.stdout)
+#        print("STDERR:")
+#        print(result.stderr)
 
-        Utils.add_attributes_to_output(logger, config['io']['l1b'], attribute_dict)
+        # readin output file
+        l1b_file = l1b_configuration['io']['l1b']
+        l1b_data = dn.DataNetCDF(logger, l1b_file, mode='r')
+
+        print("###################################################################")
+        print(f"{l1b_data}")
+        print("###################################################################")
+        if l1b_configuration['cal_level'] == 'rad':
+            print("Not detector image")
+        elif l1b_configuration['cal_level'] == 'l1b':
+            print("Not detector image")
+        else:
+            # Get data
+            data = l1b_data.get('detector_image',  group='science_data', kind = 'variable')
+            # Get dimensions from ckd?
+            ckd_file = l1b_configuration['io']['ckd']
+            ckd_data = dn.DataNetCDF(logger, ckd_file, mode='r')
+            det_rows = ckd_data.get('detector_row', kind='dimension')
+            det_cols = ckd_data.get('detector_column', kind='dimension')
+    
+            l1b_data.add(name='detector_row', value=det_rows, kind='dimension')
+            l1b_data.add(name='detector_column', value=det_cols, kind='dimension')
+            l1b_data.add(name='along_track', value=data.shape[0], kind='dimension')
+    
+            # reshape data
+            # How do I get the right dimensions when binning has been applied?
+            # For now stupidly devide det_rows by table_id
+            bin_file = l1b_configuration['io']['binning_table']
+            bin_data = dn.DataNetCDF(logger, bin_file, mode='r')
+            bin_id = l1b_configuration['detector']['binning_table_id']
+            table = f"Table_{bin_id}"
+            print(f"Binning table: {table}")
+            binned_pixels = bin_data.get('bins', group=table, kind='dimension')
+            binned_rows = int(binned_pixels/det_cols)
+            l1b_data.add(name='binned_row', value=binned_rows, kind='dimension')
+
+#            data_reshaped = np.reshape(data, (data.shape[0], det_rows, det_cols))
+            data_reshaped = np.reshape(data, (data.shape[0], binned_rows, det_cols))
+            print(f"Reshaped data shape : {data_reshaped.shape}")
+            print(f"Reshaped data image 1: {data_reshaped[1,:,:]}")
+            if l1b_configuration['cal_level'] != 'l1b':
+                l1b_data.add(name='detector_image_3d', value=data_reshaped, group='science_data', dimensions=('along_track','binned_row','detector_column'), kind='variable')
+            else:
+                l1b_data.add(name='detector_image_3d', value=data_reshaped, group='science_data', dimensions=('along_track','detector_row','detector_column'), kind='variable')
+    
+            print("###################################################################")
+            print("###################################################################")
+            print(f"{l1b_data}")
+            print("###################################################################")
+            print("###################################################################")
+            # write to temp output file
+            l1b_data.write('l1b_cpp_temp.nc')
+
+#        Utils.add_attributes_to_output(logger, l1b_configuration['io']['l1b'], attribute_dict)
 
     if step == 'l1l2' or step == 'all':
         E2EModule = importlib.import_module("L1L2.l1bl2_no2")

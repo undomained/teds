070cae8474cdae04f2c88c107b624e162c3ed60b
Mirna van Hoek <mirna.van.hoek@knmi.nl>
2024-07-10 20:46:40 +0200

    Updated to generate dark current data distributed as something of a normal distribution with a tail (lognormal) so I could try a scenario with different dark currents corresponding to different temperatures

--- teds/CKD/ckd_generation/nitro_ckds/dark/current.py
+++ teds/CKD/ckd_generation/nitro_ckds/dark/current.py
@@ -12,5 +12,14 @@ def generate(ncc):
     }
     
     dims = ['detector_row', 'detector_column']
-    dark_current = np.random.uniform(10, 360, size = ncc.get_shape(dims))
+    np.random.seed(100)
+#    dark_current = np.random.uniform(10, 360, size = ncc.get_shape(dims))
+    # Try something with a tail
+#    mean_dc = 300.
+#    sigma_dc = 100.
+    mean_dc = 50.
+    sigma_dc = 50.
+    normal_std = np.sqrt(np.log(1 + (sigma_dc/mean_dc)**2))
+    normal_mean = np.log(mean_dc) - normal_std**2 / 2
+    dark_current =  np.random.lognormal(normal_mean, normal_std, size=ncc.get_shape(dims))
     ncc.create_var_auto(dims, dark_current, attr, 'f8')

f94e899383b2d60b2547f5f49ff6d879027f1184
Benjamin Leune <benjamin.leune@knmi.nl>
2024-07-31 13:03:09 +0200

    PAM: clean-up

--- teds/PAM/pam.py
+++ teds/PAM/pam.py
@@ -1,15 +1,14 @@
 import numpy as np
 import os
 import sys
-import logging
 import yaml
 import netCDF4 as nc
 import matplotlib.pyplot as plt
 import matplotlib.colorbar as colorbar
-import matplotlib.colors as colors
 import cartopy.crs as crs
 import matplotlib as mpl
 from scipy.stats import linregress
+from pathlib import Path
 
 from teds import log
 
@@ -113,37 +112,47 @@ def plot_scatter(var1,var2,var_name,var1_name,var2_name,save_location):
     var1_flat = var1_flat[~var12_mask].data
     var2_flat = var2_flat[~var12_mask].data
 
+    # make 1D hist
+    if (var1_flat == var1_flat[0]).all():
+        plt.figure(figsize=(9,9))
+        plt.hist(var2_flat, bins=25,label='L2')
+        plt.axvline(x=var1_flat[0],linestyle='--',color='k',label='SGM')
+        plt.ylabel('Count')
+        plt.xlabel(f'{var1_name} [{var1.units}]')
+        plt.legend()
+        savestring = 'hist_'+var_name.lower().replace(" ", "_")
+        plt.savefig(f'{save_location}/{savestring}.png',format='png', dpi=1000, bbox_inches='tight')
+
+    # make 2D hist
     # fit regressions line and calc stats
-    try:
+    else:
         slope, intercept, r_pearson, p_value, std_err = linregress(var1_flat, var2_flat)
         r2 = r_pearson*r_pearson
-    except:
-        slope = intercept = r2 = -1
 
-    bias = var1_flat-var2_flat
-    sigma = np.std(bias)
-    bias = np.mean(np.abs(bias))
+        bias = var1_flat-var2_flat
+        sigma = np.std(bias)
+        bias = np.mean(np.abs(bias))
 
-    # plot
+        # plot
 
-    fig,ax = plt.subplots(figsize=(9,9))
-    h = plt.hist2d(var1_flat, var2_flat,bins=100, norm=mpl.colors.LogNorm())
-    lims = np.array([np.min([ax.get_xlim(), ax.get_ylim()]),np.max([ax.get_xlim(), ax.get_ylim()])])
-    ax.plot(lims, lims, 'k-', alpha=0.3, zorder=3,label='1:1')
-    ax.set_aspect('equal')
-    ax.set_xlim(lims)
-    ax.set_ylim(lims)
-    plt.title('N = {}, R$^2$ = {:.3f}, mean $\sigma$ = {:.3E}, mean bias = {:.3E}'.format(var1_flat.size,r2, sigma, bias))
-    plt.xlabel(f'{var1_name} [{var1.units}]')
-    plt.ylabel(f'{var2_name} [{var2.units}]')
-    plt.plot(lims,lims*slope+intercept,'k--',alpha=0.5,zorder=2,label='y={:.2f}x+{:.2E}'.format(slope, intercept))
-    plt.legend()
-    cax,kw = colorbar.make_axes(ax,location='right',pad=0.02,shrink=0.5)
-    cbar=fig.colorbar(h[-1],cax=cax, extend='neither')
-    cbar.set_label('Number of pixels')
+        fig,ax = plt.subplots(figsize=(9,9))
+        h = plt.hist2d(var1_flat, var2_flat,bins=100, norm=mpl.colors.LogNorm())
+        lims = np.array([np.min([ax.get_xlim(), ax.get_ylim()]),np.max([ax.get_xlim(), ax.get_ylim()])])
+        ax.plot(lims, lims, 'k-', alpha=0.3, zorder=3,label='1:1')
+        ax.set_aspect('equal')
+        ax.set_xlim(lims)
+        ax.set_ylim(lims)
+        plt.title('N = {}, R$^2$ = {:.3f}, mean $\sigma$ = {:.3E}, mean bias = {:.3E}'.format(var1_flat.size,r2, sigma, bias))
+        plt.xlabel(f'{var1_name} [{var1.units}]')
+        plt.ylabel(f'{var2_name} [{var2.units}]')
+        plt.plot(lims,lims*slope+intercept,'k--',alpha=0.5,zorder=2,label='y={:.2f}x+{:.2E}'.format(slope, intercept))
+        plt.legend()
+        cax,kw = colorbar.make_axes(ax,location='right',pad=0.02,shrink=0.5)
+        cbar=fig.colorbar(h[-1],cax=cax, extend='neither')
+        cbar.set_label('Number of pixels')
 
-    savestring = 'scatter_'+var_name.lower().replace(" ", "_")
-    plt.savefig(f'{save_location}/{savestring}.png',format='png', dpi=1000, bbox_inches='tight')
+        savestring = 'scatter_'+var_name.lower().replace(" ", "_")
+        plt.savefig(f'{save_location}/{savestring}.png',format='png', dpi=1000, bbox_inches='tight')
 
     return
 
@@ -157,12 +166,14 @@ def pam_nitro(cfg):
 
     sgm = read_file(cfg['io']['sgm_atm'])
     l2  = read_file(cfg['io']['l2'])
+    basedir  = os.path.dirname(cfg['io']['l2'])
 
-#    plotvars = cfg['pam']['plot_list']
+    savedir = os.path.join(basedir, 'figs' )
+    Path(savedir).mkdir(exist_ok=True)
+    
     plotvars = cfg['plot_list']
 
-#    log.info(f"Saving figures to: {cfg['pam']['figure_dir']}")
-    log.info(f"Saving figures to: {cfg['figure_dir']}")
+    log.info(f"Saving figures to: {savedir}")
 
     # loop over plotting vars
 
@@ -175,20 +186,16 @@ def pam_nitro(cfg):
         sgm_var = sgm[plotvar['sgm_name']]
 
         # map of SGM var
-#        plot_map(sgm['lat'],sgm['lon'],sgm_var, f'SGM {varname}', cfg['pam']['figure_dir'])
-        plot_map(sgm['lat'],sgm['lon'],sgm_var, f'SGM {varname}', cfg['figure_dir'])
+        plot_map(sgm['lat'],sgm['lon'],sgm_var, f'SGM {varname}', savedir)
 
         # map of L2 var
-#        plot_map(l2['lat'],l2['lon'],l2_var, f'L2 {varname}', cfg['pam']['figure_dir'])
-        plot_map(l2['lat'],l2['lon'],l2_var, f'L2 {varname}', cfg['figure_dir'])
+        plot_map(l2['lat'],l2['lon'],l2_var, f'L2 {varname}', savedir)
         
         # diff map (L2 - SGM)
-#        plot_map_diff(l2['lat'],l2['lon'],l2_var,sgm_var, varname, f'(L2 - SGM) {varname}', cfg['pam']['figure_dir'])
-        plot_map_diff(l2['lat'],l2['lon'],l2_var,sgm_var, varname, f'(L2 - SGM) {varname}', cfg['figure_dir'])
+        plot_map_diff(l2['lat'],l2['lon'],l2_var,sgm_var, varname, f'(L2 - SGM) {varname}', savedir)
 
         # scatter plot SGM vs L2 var
-#        plot_scatter(sgm_var,l2_var,varname,f'SGM {varname}',f'L2 {varname}', cfg['pam']['figure_dir'])
-        plot_scatter(sgm_var,l2_var,varname,f'SGM {varname}',f'L2 {varname}', cfg['figure_dir'])
+        plot_scatter(sgm_var,l2_var,varname,f'SGM {varname}',f'L2 {varname}', savedir)
 
     log.info(f'Finished PAM')
 

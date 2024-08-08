d8000ebd6601bcaaf2d9e9d743adcf4210cb645e
Raul Laasner <raullaasner@gmail.com>
2024-08-08 10:52:53 +0200

    Fix NetCDF library name in CI initial cache file

--- CI/initial_cache.cmake
+++ CI/initial_cache.cmake
@@ -3,5 +3,5 @@
 set(CMAKE_CXX_COMPILER g++ CACHE STRING "")
 set(CMAKE_C_COMPILER gcc CACHE STRING "")
 set(CMAKE_CXX_FLAGS_RELEASE "-O3 -march=native -DNDEBUG" CACHE STRING "")
-set(LIBRARIES netcdf_c++4 netcdf hdf5_hl hdf5 z dl blas lapack CACHE STRING "")
+set(LIBRARIES netcdf-cxx4 netcdf hdf5_hl hdf5 z dl blas lapack CACHE STRING "")
 set(TANGO_L1B_PATH build-l1b CACHE STRING "")

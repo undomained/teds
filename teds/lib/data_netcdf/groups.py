880f3b70fdb76aff5591fb474b7a9a5c63b52211
Mirna van Hoek <mirna.van.hoek@knmi.nl>
2024-08-09 21:31:48 +0200

    Updated comments, removed fcts find and remove and . These are no longer needed due to code cleanup in data_netcdf scripts

--- teds/lib/data_netcdf/groups.py
+++ teds/lib/data_netcdf/groups.py
@@ -16,21 +16,30 @@ class Group:
         - self._attribute_list
         - self._variable_list
         Methodes:
-        - __init__(self,name, value)
+        - __init__(self,name, value, level, variable_list, dimension_list, attribute_list, group_list)
+        - __str__(self)
+        - __repr__(self)
+        - __eq__(self)
+        - __ne__(self)
+        - __del__(self)
         - set_name(self, name)
         - set_level(self, level)
         - set_dimension_list(self, dimension_list)
         - set_attribute_list(self, attribute_list)
         - set_variable_list(self, variable_list)
         - set_group_list(self, group_list)
+        - set_list(self, kind, new_list, var=None)
         - get_name(self)
         - get_level(self)
         - get_dimension_list(self)
         - get_attribute_list(self)
         - get_variable_list(self)
         - get_group_list(self)
+        - get_list(self, kind, var)
         - write(self, parent)
         - read(self, parent, name)
+        - find_in_group(self,name, var, kind)
+        - find_group(self, name)
     """
 
     def __init__(self, logger, name, level=2, variable_list=None, dimension_list=None, attribute_list=None, group_list=None):
@@ -100,8 +109,6 @@ class Group:
 
     def __del__(self):
 
-        #TODO it is porbably still on the list of the parent
-        # So need to keep track of parent to be able to delete object also from parents list
         # delete corresponding variables
         for item in self._variable_list:
             del item
@@ -156,6 +163,56 @@ class Group:
             Retrun the level of the group
         """
         return self._level
+
+    def get_list(self, kind, var=None):
+        """
+           Get specified kind of list
+        """
+        if kind == 'group':
+            return self.get_group_list()
+        elif kind == 'dimension':
+            return self.get_dimension_list()
+        elif kind == 'attribute':
+            # Group attribute or variable attribute?
+            if var is not None:
+                # looking for a variable attribute!
+                for variable in self.get_variable_list():
+                   if variable.get_name() == var:
+                       return variable.get_attribute_list()
+            else:
+                # we are looking for a variable attribute!
+                return self.get_attribute_list()
+        elif kind == 'variable':
+            return self.get_variable_list()
+        else:
+            print(f"Oops, there is no list for kind: {kind}")
+
+        return None
+
+    def set_list(self, kind, new_list, var=None):
+        """
+           Set specified kind of list
+        """
+        if kind == 'group':
+            return self.set_group_list(new_list)
+        elif kind == 'dimension':
+            return self.set_dimension_list(new_list)
+        elif kind == 'attribute':
+            return self.set_attribute_list(new_list)
+            if var is not None:
+                # looking for a variable attribute!
+                for variable in self.get_variable_list():
+                   if variable.get_name() == var:
+                       return variable.set_attribute_list(new_list)
+            else:
+                # we are setting group attribute list!
+                return self.set_attribute_list(new_list)
+        elif kind == 'variable':
+            return self.set_variable_list(new_list)
+        else:
+            print(f"Oops, there is no list for kind: {kind}")
+
+        return None
 
     def set_logger(self,logger):
         """
@@ -224,23 +281,18 @@ class Group:
         else:
             group = parent.createGroup(self._name)
 
-#        self._logger.debug(f"wrting group: {self._name} to file")
-        self._logger.info(f"wrting group: {self._name} to file")
+        self._logger.debug(f"wrting group: {self._name} to file")
         for attr in self._attribute_list:
-#            self._logger.debug(f"wrting attributes: for this group")
-            self._logger.info(f"wrting attributes: for this group")
+            self._logger.debug(f"wrting attributes: for this group")
             attr.write(group)
         for dim in self._dimension_list:
-#            self._logger.debug(f"wrting dimensions: for this group")
-            self._logger.info(f"wrting dimensions: for this group")
+            self._logger.debug(f"wrting dimensions: for this group")
             dim.write(group)
         for var in self._variable_list:
-#            self._logger.debug(f"wrting variables: for this group")
-            self._logger.info(f"wrting variables: for this group")
+            self._logger.debug(f"wrting variables: for this group")
             var.write(group)
         for group_group in self._group_list:
-#            self._logger.debug("writing groups...")
-            self._logger.info("writing groups...")
+            self._logger.debug("writing groups...")
             group_group.write(group)
 
         return 
@@ -262,7 +314,6 @@ class Group:
             dim = Dimension(dimname, level=level)
             dim.read(group)
             self._dimension_list.append(dim)
-
 
         # Fill variable list
         variables = group.variables
@@ -286,7 +337,6 @@ class Group:
         level = self._level+4
         # Dive deeper if needed
         for groupname in group_groups:
-            print(f"GROUP {self._name} FOUND SUB GROUP: {groupname}")
             group_group = Group(self._logger, groupname, level=level)
             group_group.read(group)
             self._group_list.append(group_group)
@@ -297,21 +347,19 @@ class Group:
         """
             Find kind of item in the present group
         """
-
         search_list = []
-        print(f"###TRYING FINDING NAME: {name} AND KIND: {kind} and var: {var}")
         if kind == 'variable':
-         search_list = self._variable_list
+            search_list = self._variable_list
         elif kind == 'dimension':
-         search_list = self._dimension_list
+            search_list = self._dimension_list
         elif kind == 'attribute':
             if var is not None:
-                # we are looking for a variable attribute!
+                # looking for a variable attribute!
                for variable in self._variable_list:
                    if variable.get_name() == var:
-                       print(f"###IN LOOp VARIABLE: {variable} AND var: {var}")
                        return variable.find(name)
             else:
+                # looking for a group attribute!
                 search_list = self._attribute_list
         else:
             possible_kinds = ['variable', 'attribute', 'dimension']
@@ -327,15 +375,18 @@ class Group:
         return None
 
 
-    def find_group(self,name):
+    def find_group(self, name):
+        """
+           Find group with given name in group list of this group.
+           If name is of type group1/group2/group3:
+           It is checked if group1 is in group list of this group
+           and find_group is called for name=group2/group3
+        """
 
         if "/" in name:
             groups = name.split("/")
-            print(f"GROUPS: {groups}")
             groupname = groups[0]
-            print(f"groupname: {groupname}")
             group = "/".join(groups[1:] )
-            print(f"new group: {group}")
             for gr in self._group_list:
                 if gr.get_name() == groupname:
                     return gr.find_group(group)
@@ -344,161 +395,4 @@ class Group:
             for gr in self._group_list:
                 if gr.get_name() == groupname:
                     return gr
-
-
         return None
-
-#    def find(self, name, group=None, var=None, kind='variable'):
-#        """
-#            Get the kind of object (default is variable) corresponding to name name.
-#            Note: if kind is 'attribute' also variable name is needed.
-#        """
-#
-#        print(f"HIER GROUP: {group}")
-#        if group is None:
-#            # No subgroup required. Look in this group
-#            return self.find_in_group(name, var=var, kind=kind)
-#        elif "/" in group:
-#            groups = group.split("/")
-#            print(f"GROUPS: {groups}")
-#            groupname = groups[0]
-#            print(f"groupname: {groupname}")
-#            group = "/".join(groups[1:] )
-#            print(f"new group: {group}")
-#        else:
-#            groupname = group
-#            group = None
-#
-#        for gr in self._group_list:
-#            print(f"xxx HIER group name: {gr.get_name()} and groupname: {groupname}")
-#            if gr.get_name() == groupname:
-#                if kind == 'group' and name == groupname:
-#                    return gr
-#                return gr.find(name, group=group, var=var, kind=kind)
-#
-#        #If we get here the group has not been found
-#        self._logger.warning(f"Oops group with name {groupname} (and subgroup {group} is not found in group {self._name}! So {kind} with name {name} can not be found.")
-#        return None
-
-#    def remove(self, name, var=None, kind='variable'):
-#        """
-#            remove kind of item with name name from netcdf data
-#            Note: if kind is 'attribute' also variable name is needed.
-#        """
-#
-#        if group is None:
-#            if kind == 'variable':
-#             search_list = self._variable_list
-#            elif kind == 'attribute':
-#                if var is not None:
-#                    # we are looking for a variable attribute!
-#                   for variable in self._variable_list:
-#                       if variable.get_name() == var:
-#                           return variable.remove(name)
-#                else:
-#                    search_list = self._attribute_list
-#            elif kind == 'group':
-#                search_list = self._group_list
-#            else:
-#                possible_kinds = ['variable', 'attribute', 'group']
-#                self._logger.warning(f"The kind of information that you want to remove ({kind}) does not exist in netcdf. Possibilities: {possible_kinds}. Nothing removed.")
-#                return
-#
-#            found_index = 9999
-#            for index, item in enumerate(search_list):
-#                if item.get_name() == name:
-#                    found_index = index
-#                    break
-#            if found_index != 9999:
-#                del search_list[found_index]
-#                # Todo: Does this also work on original list?
-#
-#        else:
-#
-#            for gr in self._group_list:
-#                if gr.get_name() == group:
-#                    return gr.remove(name, var=var, kind=kind)
-#            #If we get here the group has not been found
-#            self._logger.warning(f"Oops group with name {group} is not found in group {self._name}. Not able to remove requested {kind} with name {name}")
-#
-#        return
-#
-
-#    def add(self, name=None, value=None, item=None, group=None, var=None, dimensions=None, attribute_list=[], kind='variable'):
-#        """
-#           Things that can be added to a group are: variables, dimensions or attributes, or other groups
-#           Can be added as a name,var combination OR as an object
-#        """
-#        if name is None and item is None:
-#            self._logger.warning("Both the name and the item arguments are not set so nothing to add")
-#            return
-#        elif name is not None and item is not None:
-#            self._logger.warning("Both the name and the item arguments are set. Not sure what to use so nothing to add")
-#            return
-#        elif item is not None:
-#
-#            if group is not None:
-#                # Add variable/dimension/attribute to sub group of group
-#                for gr in self._group_list:
-#                    if gr.get_name() == group:
-#                        gr.add(name=name, value=value, item=item, var=var, dimensions=dimensions, attribute_list=attribute_list, kind=kind)
-#                        return
-#                #If we get here the group has not been found
-#                item_type = type(item).__name__
-#                item_name = item.get_name()
-#                self._logger.warning(f"Oops group with name {group} is not found in group {self._name}. Not able to add requested item of kind {item_type} and name {item_name}")
-#            else:
-#                # Look in group
-#                # Add variable/dimension/attribute/group to group
-#                item_type = type(item).__name__
-#                item_name = item.get_name()
-#                item.set_level('group')
-#                if item_type == 'Variable':
-#                    self._variable_list.append(item)
-#                elif item_type == 'Dimensions':
-#                    self._dimension_list.append(item)
-#                elif item_type == 'Attribute':
-#                    self._attribute_list.append(item)
-#                elif item_type == 'Group':
-#                    self._group_list.append(item)
-#                elif item_type == 'Group':
-#                    self._group_list.append(item)
-#
-#        elif name is not None:
-#
-#            if group is not None:
-#
-#                # Add variable/dimension/attribute to sub group of group
-#                for gr in self._group_list:
-#                    if gr.get_name() == group:
-#                        gr.add(name, value=value, var=var, dimensions=dimensions, attribute_list=attribute_list, kind=kind)
-#                        return
-#                #If we get here the group has not been found
-#                self._logger.warning(f"Oops group with name {group} is not found in group {self._name}. Not able to add requested {kind} with name {name}")
-#
-#            else:
-#                # Add variable/dimension/attribute/group to group
-#                if kind == 'variable':
-#                    dtype = value.dtype
-#                    var = Variable(self._logger, name, value, dtype=dtype, dimensions=dimensions, attribute_list=attribute_list, level='group')
-#                    self._variable_list.append(var)
-#                elif kind == 'dimension':
-#                    dim = Dimension(name, value, level='group')
-#                    self._dimension_list.append(dim)
-#                elif kind == 'attribute':
-#                    if var is not None:
-#                       for variable in self._variable_list:
-#                           if variable.get_name() == var:
-#                               variable.add(name, value)
-#                    else:
-#                        attr = Attribute(name, value, level='group')
-#                        self._attribute_list.append(attr)
-#                elif kind == 'group':
-#                    grpr = Group(self._logger, name, level='group')
-#                    self._group_list.append(grpr)
-#                else:
-#                    possible_kinds = ['variable', 'attribute', 'dimension', 'group']
-#                    self._logger.warning(f"The kind of information that you want to add ({kind}) does not exist in netcdf. Possibilities: {possible_kinds}")
-#
-#        return
-

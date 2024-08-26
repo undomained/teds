e724ad3e1e8dda20817f24bb24038cf888e38ffd
Mirna van Hoek <mirna.van.hoek@knmi.nl>
2024-08-26 16:30:32 +0200

    Fix some typos

--- teds/l1al1b/tango_l1b/dummy_correction.h
+++ teds/l1al1b/tango_l1b/dummy_correction.h
@@ -1,39 +1,37 @@
 // This source code is licensed under the 3-clause BSD license found
 // in the LICENSE file in the root directory of this project.
 
-// Class for storing the calibration key data
-
 #pragma once
 
 #include "base_algo.h"
 
 namespace tango {
 
-/// \class PRNUCorrection
-/// \brief Correct the signal for the PRNU
+class CKD;
+class L1;
 
-class PRNUCorrection: public: BaseAlgo
-{
+/// Dummy Correction class
+
+class DummyCorrection : public BaseAlgo {
 public:
 
-    /// \brief Constructor.
-    PRNUCorrection();
+    /// Constructor.
+    DummyCorrection() = default;
 
-    /// \brief Destructor.
-    virtual ~PRNUCorrection();
+    /// Destructor.
+    ~DummyCorrection() = default;
 
     /// Return the name of the class.
-    /// \return std::string Name of the class, i.e. "PRNUCorrection".
     virtual std::string getName() const;
 
     /// Retrieve the required datasets
-    virtual void algoCheckInput(const CKD& ckd, const bool enabled, L1& l1);
+    virtual void algoCheckInput(const CKD& ckd, L1& l1);
 
-    /// \brief Set all loaded data to null.
-    virtual void unloadData();
+//    /// Set all loaded data to null.
+//    virtusl void unloadData();
 
-    /// \brief Perform the algorithm
-    /// \param [in] measurement The measurement.
+    /// Perform the algorithm
     virtual void algoExecute(const CKD& ckd, const bool enabled, L1& l1);
 
+};
 } // namespace tango

9ab6fdd8dc32ac4b95ae880fb5cbee74047011d5
Mirna van Hoek <mirna.van.hoek@knmi.nl>
2024-09-09 09:05:57 +0200

    New class L1Measurement including read and write (includes all code from io_nitro.cpp/.h)

---
+++ teds/l1al1b/tango_l1b/l1_measurement.cpp
@@ -0,0 +1,411 @@
+// This source code is licensed under the 3-clause BSD license found
+// in the LICENSE file in the root directory of this project.
+
+// Class to hold partially or fully calibrated level 1 measurements. 
+// The data level can range from L1A to L1B depending on the calibration
+
+#include "l1_measurement.h"
+#include "l1.h"
+#include "time.h"
+
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
+#include <yaml-cpp/yaml.h>
+
+namespace tango {
+
+L1Measurement::L1Measurement(const std::string filename, const std::string level, const int image_start, const int image_end, const std::string config)
+{
+
+    // When we create a L1Measurement object need to create it from file
+    // So l1_filename should be a member
+    spdlog::info("Now file_name: {}", filename);
+    l1_filename = filename;
+    l1_level = level;
+    const netCDF::NcFile nc { l1_filename, netCDF::NcFile::read };
+    spdlog::info("AFTER read NC");
+    // TODO do we want to obtain image_start and image_end from settings?
+    alt_beg = static_cast<size_t>(image_start);
+    spdlog::info("alt_beg: {}", alt_beg);
+    if (image_end == fill::i){
+        alt_end = nc.getDim("along_track").getSize();
+    } else {
+        alt_end = static_cast<size_t>(image_end) + 1;
+    }
+    spdlog::info("alt_end: {}", alt_end);
+    n_images = alt_end - alt_beg ;
+    spdlog::info("n_images: {}", n_images);
+
+    l1_measurement.resize(n_images);
+    image_time.resize(n_images);
+    binning_table.resize(n_images);
+    nr_coadditions.resize(n_images);
+    exposure_time.resize(n_images);
+
+    spdlog::info("BEFORE READING L1");
+    read(l1_filename, l1_level, config);
+    spdlog::info("AFTER READING L1");
+}
+
+void L1Measurement::read(const std::string& filename,
+    const std::string& level,
+    const std::string& config)
+{
+    l1_filename = filename;
+    l1_level = level;
+    const netCDF::NcFile nc { l1_filename, netCDF::NcFile::read };
+
+    readMetaData(nc, config);
+    if (l1_level == "L1A"){
+//    if (l1_level == "SGM"){
+        readSceneData(nc);
+    } else {
+        readScienceData(nc);
+    }
+}
+
+// Basically Edwards writeL1product fct from io.cpp
+void L1Measurement::write(const std::string& filename,
+    const std::string& level,
+    const std::string& config, 
+    const int argc, const char* const argv[]) 
+{
+    spdlog::info("write L1");
+    std::cout << filename << std::endl;
+    
+    netCDF::NcFile nc { filename, netCDF::NcFile::replace };
+
+    writeGlobalAttributes(nc, level, config,argc, argv);
+    writeMetaData(nc);
+
+//    if (level == "SGM"){
+//        writeObservationData(nc);
+//    } else {
+//        writeScienceData(nc);
+//    }
+//TODO need to fix this. Because sometime we do need to write observation data
+    writeScienceData(nc);
+
+    writeGeolocationData(nc);
+}
+
+void L1Measurement::writeGlobalAttributes(netCDF::NcFile& nc, const std::string& level, const std::string& config, const int argc, const char* const argv[]) {
+    // Edwards splitup functions copied from io.cpp
+    // TODO kunnen we niet settings gebruiken? Dat is toch al geladen config??????? Dan niet YAML::load nodig (?????)
+    spdlog::info("Writing Global Attributes");
+    const std::string instrument { YAML::Load(config)["instrument"].as<std::string>() };
+    nc.putAtt("Conventions", "CF-1.11");
+    nc.putAtt("title", "Tango " + instrument + " " + level + " data");
+    nc.putAtt("product_type", level);
+    nc.putAtt("project", "TANGO");
+    nc.putAtt("instrument", instrument);
+    nc.putAtt("creator_name", "SRON/Earth Science");
+    nc.putAtt("creator_url", "https://www.sron.nl/missions-earth");
+    nc.putAtt("date_created", getDateAndTime());
+
+    if (TANGO_GIT_COMMIT_ABBREV != "GITDIR-N") {
+        nc.putAtt("git_commit", TANGO_GIT_COMMIT_ABBREV);
+    }
+
+    std::string command_line { argc > 0 ? argv[0] : "" };
+    for (int i { 1 }; i < argc; ++i) {
+        command_line += ' ' + std::string(argv[i]);
+    }
+    nc.putAtt("history", command_line);
+    const std::string processing_version { YAML::Load(config)["processing_version"].as<std::string>() };
+    nc.putAtt("processing_version", processing_version);
+}
+
+void L1Measurement::writeMetaData(netCDF::NcFile& nc) {
+    // Edwards splitup functions copied from io.cpp with l1_products -> l1_measurement
+    spdlog::info("Writing Image Attributes");
+//    const auto n_images { l1_products.size() };
+    const auto nc_images { nc.addDim("along_track", n_images) };
+
+    auto nc_grp { nc.addGroup("image_attributes") };
+
+    NcVar nc_img_time = addVariable(nc_grp, "image_time", "image time", "seconds since 2022-03-21", fill::d, 0.0, 92304.0, {nc_images});
+    std::vector<double> buf {};
+    buf.assign(n_images, 0.0);
+    nc_img_time.putVar(buf.data());
+
+    auto nc_var = nc_grp.addVar("binning_table", netCDF::ncByte, { nc_images });
+    nc_var.putAtt("long_name", "binning table");
+    buf.assign(n_images, l1_measurement.front().binning_table_id);
+    nc_var.putVar(buf.data());
+
+    nc_var = nc_grp.addVar("nr_coadditions", netCDF::ncUshort, { nc_images });
+    nc_var.putAtt("long_name", "number of coadditions");
+    buf.assign(n_images, l1_measurement.front().nr_coadditions);
+    nc_var.putVar(buf.data());
+
+    nc_var = nc_grp.addVar("exposure_time", netCDF::ncDouble, { nc_images });
+    nc_var.putAtt("long_name", "exposure time");
+    buf.assign(n_images, l1_measurement.front().exposure_time);
+    nc_var.putVar(buf.data());
+
+}
+
+void L1Measurement::writeObservationData(netCDF::NcFile& nc) {
+    // Edwards splitup functions copied from io.cpp with l1_products -> l1_measurement
+    spdlog::info("Writing Observation Data");
+    const auto n_across_track { l1_measurement.front().observation_sig.size() };    
+    const auto nc_across_track { nc.addDim("across_track", n_across_track) };
+//    const auto n_images = l1_measurement.size();
+    const auto nc_images = nc.getDim("along_track");
+    auto& l1 { l1_measurement[0] };
+
+    const auto& wavelengths = *l1_measurement.front().wavelength; 
+    const auto n_wavelength { wavelengths.front().size() };
+    const auto nc_wavelength { nc.addDim("wavelength", n_wavelength) };
+    
+    auto nc_grp { nc.addGroup("observation_data") };
+
+    // add wavelength
+    NcVar nc_wl = addVariable(nc_grp, "wavelength", "radiance wavelengths", "nm", fill::f, 0.0f, 999.0f, {nc_across_track, nc_wavelength});
+    std::vector<float> buf(n_across_track * n_wavelength);
+    for (size_t i {}; i < n_across_track; ++i) {
+        for (size_t j {}; j < n_wavelength; ++j) {
+            buf[i * n_wavelength + j] = static_cast<float>(wavelengths[i][j]);
+        }
+    }
+    nc_wl.putVar(buf.data());
+
+    // add radiance, flatten first
+    const auto flatsignal { [&](auto& buf) {
+        for (size_t i {}; i < n_images; ++i) {
+            for (size_t j {}; j < n_across_track; ++j) {
+                for (size_t k {}; k < n_wavelength; ++k) {
+                    buf[(i * n_across_track + j) * n_wavelength + k] = static_cast<float>(l1_measurement[i].observation_sig[j][k]);
+                }
+            }
+        }
+    } };
+    NcVar nc_rad = addVariable(nc_grp, "radiance", "radiance", "ph nm-1 s-1 sr-1 m-2", fill::f, 0.0f, 1e20f, {nc_images, nc_across_track, nc_wavelength});
+    flatsignal(buf);
+    nc_rad.putVar(buf.data());
+
+    const auto flatstd { [&](auto& buf) {
+        for (size_t i {}; i < n_images; ++i) {
+            for (size_t j {}; j < n_across_track; ++j) {
+                for (size_t k {}; k < n_wavelength; ++k) {
+                    buf[(i * n_across_track + j) * n_wavelength + k] = static_cast<float>(l1_measurement[i].observation_std[j][k]);
+                }
+            }
+        }
+    }}; 
+    
+    NcVar nc_std = addVariable(nc_grp, "radiance_stdev", "standard deviation of radiance in bin", "ph nm-1 s-1 sr-1 m-2", fill::f, 0.0f, 1e20f, {nc_images, nc_across_track, nc_wavelength});
+    flatstd(buf);
+    nc_std.putVar(buf.data());
+}
+
+void L1Measurement::writeScienceData(netCDF::NcFile& nc){
+    // Edwards splitup functions copied from io.cpp with l1_products -> l1_measurement
+    spdlog::info("Writing Science Data");
+// Note: n_images is member of L1Measurements
+//    const auto n_images { l1_products.size() };
+    const auto n_bins { l1_measurement.front().image.size() };
+
+    const auto nc_images = nc.getDim("along_track");
+    const auto nc_detector_bin { nc.addDim("detector_bin", n_bins) };
+
+    const auto n_rows {(*l1_measurement.front().wavelength).size()};
+    const auto nc_rows { nc.addDim("rows", n_rows) };
+
+    const auto& wavelengths = *l1_measurement.front().wavelength; 
+    const auto n_cols { wavelengths.front().size() };
+    const auto nc_cols { nc.addDim("cols", n_cols) };
+
+
+    const std::vector<netCDF::NcDim> nc_flatimg_shape { nc_images, nc_detector_bin };
+    
+    auto nc_grp { nc.addGroup("science_data") };
+
+    // add image
+    NcVar nc_img = addVariable(nc_grp, "detector_image", "detector images", "counts" , fill::d, -1e100, 1e100, nc_flatimg_shape);
+    std::vector<double> buf(n_images * n_bins);
+    for (size_t i {}; i < n_images; ++i) {
+        for (size_t j {}; j < n_bins; ++j) {
+            buf[i * n_bins + j] = l1_measurement[i].image[j];
+        }
+    }
+    nc_img.putVar(buf.data());
+
+    // add stdev
+    NcVar nc_std = addVariable(nc_grp, "detector_stdev", "standard deviation of detector bin","counts", fill::d, 0.0,1e100, nc_flatimg_shape);
+    for (size_t i {}; i < n_images; ++i) {
+        for (size_t j {}; j < n_bins; ++j) {
+            buf[i * n_bins + j] = l1_measurement[i].stdev[j];
+        }
+    }
+    nc_std.putVar(buf.data());
+
+    // Add wavelength
+    NcVar nc_wl = addVariable(nc_grp, "wavelength", "radiance wavelengths", "nm", fill::f, 0.0f, 999.0f, {nc_rows, nc_cols});
+    std::vector<float> buf2(n_rows * n_cols);
+    for (size_t i {}; i < n_rows; ++i) {
+        for (size_t j {}; j < n_cols; ++j) {
+            buf2[i * n_cols + j] = static_cast<float>(wavelengths[i][j]);
+        }
+    }
+    nc_wl.putVar(buf2.data());
+}
+
+void L1Measurement::writeGeolocationData(netCDF::NcFile& nc) {
+    // Edwards splitup functions copied from io.cpp with l1_products -> l1_measurement
+    spdlog::info("Writing Geolocation Data");
+
+    const auto n_across_track { l1_measurement.front().spectra.size() };
+    const auto nc_across_track { nc.addDim("across_track", n_across_track) };
+// Note: n_images is member of L1Measurements
+//    const auto n_images { l1_products.size() };
+    const auto nc_images = nc.getDim("along_track");
+    const std::vector<netCDF::NcDim> geometry_shape { nc_images, nc_across_track };
+
+    std::vector<float> lat(n_images * n_across_track);
+    std::vector<float> lon(n_images * n_across_track);
+    std::vector<float> height(n_images * n_across_track);
+    std::vector<float> vza(n_images * n_across_track);
+    std::vector<float> vaa(n_images * n_across_track);
+    std::vector<float> sza(n_images * n_across_track);
+    std::vector<float> saa(n_images * n_across_track);
+    for (int i_alt {}; i_alt < n_images; ++i_alt) {
+        const auto copy { [i_alt](const std::vector<float>& in,
+                                    std::vector<float>& out) {
+            std::copy(
+                in.cbegin(), in.cend(), out.begin() + i_alt * in.size());
+        } };
+        copy(l1_measurement[i_alt].geo.lat, lat);
+        copy(l1_measurement[i_alt].geo.lon, lon);
+        copy(l1_measurement[i_alt].geo.height, height);
+        copy(l1_measurement[i_alt].geo.vza, vza);
+        copy(l1_measurement[i_alt].geo.vaa, vaa);
+        copy(l1_measurement[i_alt].geo.sza, sza);
+        copy(l1_measurement[i_alt].geo.saa, saa);
+    }
+
+    auto nc_grp = nc.addGroup("geolocation_data");
+
+    NcVar nc_lat = addVariable(nc_grp, "latitude", "latitude at bin locations", "degrees_north", fill::f, -90.0f, 90.0f, geometry_shape);
+    NcVar nc_lon = addVariable(nc_grp, "longitude", "longitude at bin locations", "degrees_east", fill::f, -180.0f, 180.0f, geometry_shape);
+    NcVar nc_hei = addVariable(nc_grp, "height", "height at bin locations", "m", fill::f, -1000.0f, 1000.0f, geometry_shape);
+    NcVar nc_vza = addVariable(nc_grp, "viewingzenithangle", "sensor zenith angle at bin locations", "degrees", fill::f, -90.0f, 90.0f, geometry_shape);
+    NcVar nc_vaa = addVariable(nc_grp, "viewingazimuthangle", "sensor azimuth angle at bin locations", "degrees", fill::f, -180.0f, 180.0f, geometry_shape);
+    NcVar nc_sza = addVariable(nc_grp, "solarzenithangle", "solar zenith angle at bin locations", "degrees", fill::f, -90.0f, 90.0f, geometry_shape);
+    NcVar nc_saa = addVariable(nc_grp, "solarazimuthangle", "solar azimuth angle at bin locations", "degrees", fill::f, -180.0f, 180.0f, geometry_shape);
+    
+    nc_lon.putVar(lat.data());
+    nc_lat.putVar(lat.data());
+    nc_hei.putVar(height.data());
+    nc_vza.putVar(vza.data());
+    nc_vaa.putVar(vaa.data());
+    nc_sza.putVar(sza.data());
+    nc_saa.putVar(saa.data());
+
+}
+
+void L1Measurement::readMetaData(const netCDF::NcFile& nc, const std::string& config) {
+    // Edwards Meta data part from readL1 fct from io_nitro.cpp?
+    spdlog::info("Reading Metadata");
+    if (l1_level == "L1A") {
+        uint8_t detector_binning_table_id { YAML::Load(config)["detector"]["binning_table_id"].as<uint8_t>() };
+        uint16_t nr_coadditions { YAML::Load(config)["detector"]["nr_coadditions"].as<uint16_t>() };
+        float exposure_time { YAML::Load(config)["detector"]["exposure_time"].as<float>() };
+        for (int i_alt {}; i_alt < static_cast<int>(n_images); ++i_alt) {
+//            l1_measurement[i_alt].binning_table_id = static_cast<uint8_t>(settings.detector.binning_table_id)[i_alt];
+//            l1_measurement[i_alt].nr_coadditions = static_cast<uint16_t>(settings.detector.nr_coadditions);
+//            l1_measurement[i_alt].exposure_time =  settings.detector.exposure_time;
+            l1_measurement[i_alt].binning_table_id =  detector_binning_table_id;
+            l1_measurement[i_alt].nr_coadditions = nr_coadditions;
+            l1_measurement[i_alt].exposure_time =  exposure_time;
+        }
+    } else {
+        if (const auto grp { nc.getGroup("image_attributes") }; !grp.isNull()) {
+            grp.getVar("image_time").getVar({ alt_beg }, { n_images }, image_time.data());
+            grp.getVar("binning_table").getVar({ alt_beg }, { n_images }, binning_table.data());
+            grp.getVar("nr_coadditions").getVar({ alt_beg }, { n_images }, nr_coadditions.data());
+            grp.getVar("exposure_time").getVar({ alt_beg }, { n_images }, exposure_time.data());
+            for (int i_alt {}; i_alt < static_cast<int>(n_images); ++i_alt) {
+                l1_measurement[i_alt].image_time = image_time[i_alt];
+                l1_measurement[i_alt].binning_table_id = binning_table[i_alt];
+                l1_measurement[i_alt].nr_coadditions = nr_coadditions[i_alt];
+                l1_measurement[i_alt].exposure_time = exposure_time[i_alt];
+            }
+        }
+    }
+    
+}
+
+void L1Measurement::readScienceData(const netCDF::NcFile& nc) {
+    // Edwards Science data part from readL1 fct from io_nitrp.cpp
+    spdlog::info("Reading Science Data");
+    const auto grp { nc.getGroup("science_data") };
+    const auto n_bins { nc.getDim("detector_bin").getSize() }; // == rows * cols
+    const auto n_rows { nc.getDim("rows").getSize() };
+    const auto n_cols { nc.getDim("cols").getSize() };
+
+    std::vector<double> detector_images(n_images * n_bins);
+    std::vector<double> detector_stdev(n_images * n_bins);
+    std::vector<double> flattened_wl(n_bins);
+
+    grp.getVar("detector_image").getVar({ alt_beg, 0 }, { n_images, n_bins }, detector_images.data());
+    grp.getVar("detector_stdev").getVar({ alt_beg, 0 }, { n_images, n_bins }, detector_stdev.data());
+    grp.getVar("wavelength").getVar(flattened_wl.data());
+    for (size_t i_alt {}; i_alt < n_images; ++i_alt) {
+        auto& l1 { l1_measurement[i_alt] };
+        l1.image.resize(n_bins);
+        l1.stdev.resize(n_bins);
+        for (size_t i {}; i < n_bins; ++i) {
+            l1.image[i] = detector_images[i_alt * n_bins + i];
+        }
+    }
+    
+    // Import wavelength
+    auto& l1 {l1_measurement.front()};
+    std::vector<std::vector<double>> wavelength2D(n_rows, std::vector<double>(n_cols));
+    for (int i {}; i < n_rows; ++i) {
+        for (int j {}; j < n_cols; ++j) {
+            wavelength2D[i][j] = flattened_wl[i * n_cols + j];
+        }
+    } 
+    l1.wavelength = std::make_shared<std::vector<std::vector<double>>>(wavelength2D);
+}
+
+void L1Measurement::readSceneData(const netCDF::NcFile& nc){
+    // Edwards read Scene data part from readL1 fct from io_nitro.cpp
+    spdlog::info("Reading Scene Data");
+    const auto n_act { nc.getDim("across_track").getSize() };
+    const auto n_wavelength { nc.getDim("wavelength").getSize() };
+    std::vector<double> spectra(n_images * n_act * n_wavelength);
+    std::vector<double> wavelength(n_wavelength);
+
+    nc.getVar("radiance").getVar({ alt_beg, 0, 0 }, { n_images, n_act, n_wavelength }, spectra.data());
+    nc.getVar("wavelength").getVar(wavelength.data());
+
+    // import wavelength
+    auto wavelength_lbl {std::make_shared<std::vector<std::vector<double>>>(n_act, std::vector<double>(n_wavelength))};
+    for (size_t i_act {}; i_act < n_act; ++i_act) {
+        for (size_t i {}; i < n_wavelength; ++i) {
+            (*wavelength_lbl)[i_act][i] = wavelength[i];
+        }
+    }
+
+    // set first l1 wavelength, is shared between other l1 objects
+    l1_measurement.front().wavelength = wavelength_lbl;
+
+    //import spectra
+    for (size_t i_alt {}; i_alt < n_images; ++i_alt) {
+        auto& l1 { l1_measurement[i_alt] };
+        l1.observation_sig.resize(n_act);
+        for (size_t i_act {}; i_act < n_act; ++i_act) {
+            l1.observation_sig[i_act].resize(n_wavelength);
+            for (size_t i {}; i < n_wavelength; ++i) {
+                const size_t idx { (i_alt * n_act + i_act) * n_wavelength + i };
+                l1.observation_sig[i_act][i] = spectra[idx];
+            }
+        }
+    }
+}
+
+} // namespace tango
---
+++ teds/l1al1b/tango_l1b/l1_measurement.h
@@ -0,0 +1,107 @@
+// This source code is licensed under the 3-clause BSD license found
+// in the LICENSE file in the root directory of this project.
+
+#pragma once
+
+#include "constants.h"
+#include "spectrum.h"
+#include "setting.h"
+#include <netcdf>
+#include <memory>
+
+namespace tango {
+
+class L1;
+
+class L1Measurement {
+private:
+    void readMetaData(const netCDF::NcFile& nc, const std::string& config);
+    void readScienceData(const netCDF::NcFile& nc);
+    void readSceneData(const netCDF::NcFile& nc);
+    void writeGlobalAttributes(netCDF::NcFile& nc, const std::string& level, const std::string& config, const int argc, const char* const argv[]);
+    void writeMetaData(netCDF::NcFile& nc);
+    void writeScienceData(netCDF::NcFile& nc);
+    void writeObservationData(netCDF::NcFile& nc);
+    void writeGeolocationData(netCDF::NcFile& nc);
+
+    // Template function to get netCDF type from standard dtype
+    template<typename T>
+    netCDF::NcType getNetCDFType() {
+        if constexpr (std::is_same<T, double>::value) {
+            return netCDF::ncDouble;
+        } else if constexpr (std::is_same<T, float>::value) {
+            return netCDF::ncFloat;
+        } else if constexpr (std::is_same<T, int>::value) {
+            return netCDF::ncInt;
+        } else if constexpr (std::is_same<T, unsigned short>::value) {
+            return netCDF::ncUshort;
+        } else if constexpr (std::is_same<T, char>::value) {
+            return netCDF::ncByte;
+        }
+    }
+    
+    typedef netCDF::NcVar NcVar;
+    
+    // Template function for adding variables to netcdf
+    template<typename T>
+    netCDF::NcVar addVariable(    
+        netCDF::NcGroup& nc_grp, 
+        const std::string& varname, 
+        const std::string& long_name,
+        const std::string& units,  
+        T fill_value, 
+        T min_val,
+        T max_val,
+        const std::vector<netCDF::NcDim>& dims) 
+    {
+        // Use the helper function to get the appropriate netCDF type
+        netCDF::NcType nc_type = getNetCDFType<T>();
+    
+        // Create the variable
+        netCDF::NcVar nc_var = nc_grp.addVar(varname, nc_type, dims);
+    
+        // Set attributes
+        nc_var.putAtt("_FillValue", nc_type, fill_value);
+        nc_var.putAtt("long_name", long_name);
+        nc_var.putAtt("units", units);
+        nc_var.putAtt("valid_min", nc_type, min_val);
+        nc_var.putAtt("valid_max", nc_type, max_val);
+    
+        return nc_var;
+    }
+
+public:
+    /// Constructor.
+    L1Measurement(
+            const std::string filename,
+            const std::string level, 
+            const int image_start,
+            const int image_end,
+            const std::string config);
+
+    /// Destructor.
+    ~L1Measurement() = default;
+
+    void read(const std::string& filename,
+        const std::string& level,
+        const std::string& config);
+
+    void write(const std::string& filename,
+        const std::string& level,
+        const std::string& config, 
+        const int argc, const char* const argv[]);
+
+    std::string l1_filename;
+    std::string l1_level;
+    std::vector<L1> l1_measurement;
+    std::vector<double> image_time;
+    std::vector<uint8_t> binning_table;
+    std::vector<uint16_t> nr_coadditions;
+    std::vector<double> exposure_time;
+    size_t alt_beg {};
+    size_t alt_end {};
+    size_t n_images {};
+};
+
+
+} // namespace tango

ff3de49ff90c3388211f030502c5cddd0bf00997
Raul Laasner <raullaasner@gmail.com>
2024-09-30 17:19:36 +0200

    Export gen_ckd from Carbon CKD generator
    Also in ckd_generator_carbon.py:
     * update dimensions
     * update docstring style
     * add type hints
     * update function names to have underlines

--- teds/ckd/ckd_generation/ckd_generator_carbon.py
+++ teds/ckd/ckd_generation/ckd_generator_carbon.py
@@ -18,6 +18,7 @@ from scipy.interpolate import bisplrep
 from scipy.interpolate import bisplev
 from scipy.interpolate import CubicSpline
 from scipy.interpolate import PchipInterpolator
+from teds import log
 import numpy as np
 import yaml
 import sys
@@ -25,18 +26,21 @@ import sys
 default_fill_value = -32767
 
 
-def genHeader(conf, nc_ckd):
-    '''Generate basic CKD dimensions and global attributes.
+def gen_header(conf: dict, nc_ckd: Dataset) -> None:
+    """Generate basic CKD dimensions and global attributes.
 
     Parameters
     ----------
-      conf - settings read from the YAML file
-      nc_ckd - NetCDF file to save the result to
-    '''
+    conf
+        settings read from the YAML file
+    nc_ckd
+        NetCDF file to save the result to
+
+    """
     # Basic detector dimensions
     nc_ckd.createDimension('detector_row', conf['main']['n_detector_rows'])
     nc_ckd.createDimension('detector_column', conf['main']['n_detector_cols'])
-    nc_ckd.createDimension('across_track', 100)
+    nc_ckd.createDimension('across_track_sample', conf['main']['n_act'])
     nc_ckd.createDimension('vector', 3)
 
     # Global attributes
@@ -47,14 +51,17 @@ def genHeader(conf, nc_ckd):
     nc_ckd.creator_name = "SRON/Earth Science"
 
 
-def genDark(conf, nc_ckd):
-    '''Generate detector dark CKD and save into a NetCDF file.
+def gen_dark(conf: dict, nc_ckd: Dataset) -> None:
+    """Generate detector dark CKD and save into a NetCDF file.
 
     Parameters
     ----------
-      conf - settings read from the YAML file
-      nc_ckd - NetCDF file to save the result to
-    '''
+    conf
+        settings read from the YAML file
+    nc_ckd
+        NetCDF file to save the result to
+
+    """
     nc_grp = nc_ckd.createGroup('dark')
     nc_ckd_in = Dataset(conf['dark']['ckd_in'])
     nc_var = nc_grp.createVariable(
@@ -76,14 +83,17 @@ def genDark(conf, nc_ckd):
     nc_var[:] = spline(conf['main']['temperature'])
 
 
-def genNoise(conf, nc_ckd):
-    '''Generate detector noise CKD and save into a NetCDF file.
+def gen_noise(conf: dict, nc_ckd: Dataset) -> None:
+    """Generate detector noise CKD and save into a NetCDF file.
 
     Parameters
     ----------
-      conf - settings read from the YAML file
-      nc_ckd - NetCDF file to save the result to
-    '''
+    conf
+        settings read from the YAML file
+    nc_ckd
+        NetCDF file to save the result to
+
+    """
     nc_grp = nc_ckd.createGroup('noise')
     nc_ckd_in = Dataset(conf['dark']['ckd_in'])
     nc_var = nc_grp.createVariable(
@@ -105,14 +115,17 @@ def genNoise(conf, nc_ckd):
     nc_var[:] = spline(conf['main']['temperature'])
 
 
-def genNonlin(conf, nc_ckd):
-    '''Generate detector nonlinearity CKD and save into a NetCDF file.
+def gen_nonlin(conf: dict, nc_ckd: Dataset) -> None:
+    """Generate detector nonlinearity CKD and save into a NetCDF file.
 
     Parameters
     ----------
-      conf - settings read from the YAML file
-      nc_ckd - NetCDF file to save the result to
-    '''
+    conf
+        settings read from the YAML file
+    nc_ckd
+        NetCDF file to save the result to
+
+    """
     nc_grp = nc_ckd.createGroup('nonlinearity')
     n_knots = 100
     nc_grp.createDimension('knots', n_knots)
@@ -126,18 +139,21 @@ def genNonlin(conf, nc_ckd):
     nc_var[:] = np.linspace(-100, 10000, n_knots)
 
 
-def genSwathSpectral(conf, nc_ckd):
-    '''Generate swath and spectral CKD and save into a NetCDF file.
+def gen_swath_spectral(conf: dict, nc_ckd: Dataset) -> None:
+    """Generate swath and spectral CKD and save into a NetCDF file.
 
     Parameters
     ----------
-      conf - settings read from the YAML file
-      nc_ckd - NetCDF file to save the result to
-    '''
+    conf
+        settings read from the YAML file
+    nc_ckd
+        NetCDF file to save the result to
+
+    """
     act = (-1.7146, -1.1409, -0.56762, 0.00584267, 0.57931, 1.1526, 1.7262)
     wavelengths = (1.59, 1.61, 1.6325, 1.65, 1.675)
     wavelengths_new = (1.585, 1.61, 1.6325, 1.65, 1.679)
-    row_indices = [
+    row_indices_in = [
         [-3.760067, -3.755833, -3.749533, -3.746533, -3.7609],
         [-2.511, -2.508033, -2.5034, -2.500633, -2.508167],
         [-1.257133, -1.2556, -1.253167, -1.251533, -1.2547],
@@ -146,7 +162,7 @@ def genSwathSpectral(conf, nc_ckd):
         [2.510867, 2.5079, 2.5033, 2.5006, 2.5082],
         [3.7599, 3.755733, 3.749467, 3.746533, 3.7609],
        ]
-    col_indices = [
+    col_indices_in = [
         [3.9143, 1.9941, 0.0393533, -1.961433, -4.54967],
         [4.041167, 2.1146, 0.1542133, -1.85, -4.441167],
         [4.1177, 2.1873, 0.22349, -1.7841, -4.3759],
@@ -156,8 +172,14 @@ def genSwathSpectral(conf, nc_ckd):
         [3.9144, 1.9942, 0.03944433, -1.961367, -4.549567],
        ]
     pixel_size = 0.015  # mm
-    row_indices = np.asarray(row_indices) / pixel_size
-    col_indices = np.asarray(col_indices) / pixel_size
+    row_indices = np.asarray(row_indices_in) / pixel_size
+    col_indices = np.asarray(col_indices_in) / pixel_size
+    # The spot measurement data assumes a 512x640 detector. For any
+    # other dimensions the generated pixel indices need to be scaled
+    # appropriately.
+    row_indices *= conf['main']['n_detector_rows'] / 512
+    col_indices *= conf['main']['n_detector_cols'] / 640
+    # Shift to the detector center
     row_indices += conf['main']['n_detector_rows'] / 2
     col_indices += conf['main']['n_detector_cols'] / 2
 
@@ -172,18 +194,18 @@ def genSwathSpectral(conf, nc_ckd):
                           row_indices.flatten(),
                           kx=3,
                           ky=3)
-    n_act = 100
     min_act = -1.714
     max_act = 1.714
-    act_angles = np.linspace(min_act, max_act, n_act)
+    act_angles = np.linspace(
+        min_act, max_act, nc_ckd.dimensions['across_track_sample'].size)
     row_indices = bisplev(act_angles, np.arange(0.0, 640.0, 1.0), los_spline)
     nc_grp = nc_ckd.createGroup('swath')
     nc_var = nc_grp.createVariable(
-        'row_index', 'f8', ('across_track', 'detector_column'))
+        'row_index', 'f8', ('across_track_sample', 'detector_column'))
     nc_var.long_name = 'row indices at which to extract the spectra'
     nc_var[:] = np.flip(np.flip(row_indices, 0), 1)
     nc_var = nc_grp.createVariable(
-        'line_of_sight', 'f8', ('across_track', 'vector'))
+        'line_of_sight', 'f8', ('across_track_sample', 'vector'))
     nc_var[:] = (1.0, 0.0, 0.0)
     nc_var.long_name = 'line of sight vectors'
     # Spectral
@@ -195,26 +217,29 @@ def genSwathSpectral(conf, nc_ckd):
     wavelength = bisplev(act_angles, np.arange(0.0, 640.0, 1.0), wave_spline)
     nc_grp = nc_ckd.createGroup('spectral')
     nc_var = nc_grp.createVariable(
-        'wavelength', 'f8', ('across_track', 'detector_column'))
+        'wavelength', 'f8', ('across_track_sample', 'detector_column'))
     nc_var.long_name = 'wavelengths of L1B spectra'
     nc_var.units = 'nm'
     wavelength *= 1e3
     nc_var[:] = wavelength
 
 
-def genPRNU(conf, nc_ckd):
-    '''Generate PRNU x QE CKD and save into a NetCDF file.
+def gen_prnu(conf: dict, nc_ckd: Dataset) -> None:
+    """Generate PRNU x QE CKD and save into a NetCDF file.
 
     Parameters
     ----------
-      conf - settings read from the YAML file
-      nc_ckd - NetCDF file to save the result to
-    '''
+    conf
+        settings read from the YAML file
+    nc_ckd
+        NetCDF file to save the result to
+
+    """
     nc_ckd_in = Dataset(conf['prnu']['ckd_in'])
     # Number of wavelengths for quantum efficiency (QE)
     n_waves = nc_ckd_in.dimensions['wavelength'].size
     # Number of L1B spectra
-    n_act = nc_ckd.dimensions['across_track'].size
+    n_act = nc_ckd.dimensions['across_track_sample'].size
     # QE is more difficult because it is provided per wavelength.
     # First step is to interpolate QE onto the target temperature.
     qe = np.zeros(n_waves)
@@ -227,8 +252,8 @@ def genPRNU(conf, nc_ckd):
     qe_spline = CubicSpline(nc_ckd_in['wavelength'][:], qe)
     spectra = np.zeros((n_act, conf['main']['n_detector_cols']))
     wavelength = nc_ckd['spectral/wavelength'][:]
-    for i_fov in range(n_act):
-        spectra[i_fov, :] = qe_spline(wavelength[i_fov, :])
+    for i_act in range(n_act):
+        spectra[i_act, :] = qe_spline(wavelength[i_act, :])
     # At this point, we have a QE value for each spectrum (FOV) index and
     # each detector column. Final step is to interpolate, per column, the
     # QE values from FOV indices to detector rows using the FOV CKD.
@@ -236,7 +261,7 @@ def genPRNU(conf, nc_ckd):
     qe_map = np.zeros((conf['main']['n_detector_rows'],
                        conf['main']['n_detector_cols']))
     # Interpolation target is a list of integer row numbers
-    row_indices_out = np.arange(0.0, 512.0, 1.0)
+    row_indices_out = np.arange(0.0, conf['main']['n_detector_rows'], 1.0)
     row_indices = nc_ckd['swath/row_index'][:]
     row_indices = np.flip(row_indices, 0)
     for i_col in range(conf['main']['n_detector_cols']):
@@ -258,14 +283,17 @@ def genPRNU(conf, nc_ckd):
     nc_var[:] = prnu
 
 
-def genStray(conf, nc_ckd):
-    '''Generate stray light CKD and save into a NetCDF file.
+def gen_stray(conf: dict, nc_ckd: Dataset) -> None:
+    """Generate stray light CKD and save into a NetCDF file.
 
     Parameters
     ----------
-      conf - settings read from the YAML file
-      nc_ckd - NetCDF file to save the result to
-    '''
+    conf
+        settings read from the YAML file
+    nc_ckd
+        NetCDF file to save the result to
+
+    """
     nc_grp = nc_ckd.createGroup('stray')
     nc_ckd_in = Dataset(conf['stray']['ckd_in'])['stray']
 
@@ -307,30 +335,35 @@ def genStray(conf, nc_ckd):
     nc_var[:] = nc_ckd_in['edges'][:]
 
 
-def genRadiometric(conf, nc_ckd):
-    '''Generate radiometric CKD and save into a NetCDF file.
+def gen_radiometric(conf: dict, nc_ckd: Dataset) -> None:
+    """Generate radiometric CKD and save into a NetCDF file.
 
     Parameters
     ----------
-      conf - settings read from the YAML file
-      nc_ckd - NetCDF file to save the result to
-    '''
+    conf
+        settings read from the YAML file
+    nc_ckd
+        NetCDF file to save the result to
+
+    """
     nc_grp = nc_ckd.createGroup('radiometric')
     nc_var = nc_grp.createVariable(
-        'radiometric', 'f8', ('across_track', 'detector_column'))
+        'radiometric', 'f8', ('across_track_sample', 'detector_column'))
     nc_var.long_name = 'radiometric calibration constant'
     nc_var[:] = 4222381848451.05
 
 
-def genPixelMask(conf, nc_ckd):
-    '''Generate detector bad pixel mask and save into a NetCDF file.
+def gen_pixel_mask(conf: dict, nc_ckd: Dataset) -> None:
+    """Generate detector bad pixel mask and save into a NetCDF file.
 
     Parameters
     ----------
-      conf - settings read from the YAML file
-      nc_ckd - NetCDF file to save the result to
-    '''
+    conf
+        settings read from the YAML file
+    nc_ckd
+        NetCDF file to save the result to
 
+    """
     nc_var = nc_ckd.createVariable('pixel_mask',
                                    'b',
                                    ('detector_row', 'detector_column'),
@@ -345,39 +378,47 @@ def genPixelMask(conf, nc_ckd):
     nc_var[:] = mask
 
 
+def gen_ckd(conf: dict) -> None:
+    print('##############################\n'
+          '# Tango Carbon CKD generator #\n'
+          '##############################')
+
+    nc_ckd = Dataset(conf['main']['ckd_out'], 'w')
+
+    log.info('Generating dimensions and global attributes')
+    gen_header(conf, nc_ckd)
+
+    print('Generating dark offset and current')
+    gen_dark(conf, nc_ckd)
+
+    print('Generating noise CKD')
+    gen_noise(conf, nc_ckd)
+
+    print('Generating nonlinearity CKD')
+    gen_nonlin(conf, nc_ckd)
+
+    print('Generating swath and spectral CKD')
+    gen_swath_spectral(conf, nc_ckd)
+
+    print('Generating PRNU and quantum efficiency CKD')
+    gen_prnu(conf, nc_ckd)
+
+    print('Generating stray light CKD')
+    gen_stray(conf, nc_ckd)
+
+    print('Generating radiometric CKD')
+    gen_radiometric(conf, nc_ckd)
+
+    print('Generating detector bad pixel mask')
+    gen_pixel_mask(conf, nc_ckd)
+
+    nc_ckd.close()
+
+
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('usage: ckd_generator.py [ckd.yaml]')
         exit(0)
 
     conf = yaml.safe_load(open(sys.argv[1]))
-    nc_ckd = Dataset(conf['main']['ckd_out'], 'w')
-
-    print('Generating dimensions and global attributes')
-    genHeader(conf, nc_ckd)
-
-    print('Generating dark offset and current')
-    genDark(conf, nc_ckd)
-
-    print('Generating noise CKD')
-    genNoise(conf, nc_ckd)
-
-    print('Generating nonlinearity CKD')
-    genNonlin(conf, nc_ckd)
-
-    print('Generating swath and spectral CKD')
-    genSwathSpectral(conf, nc_ckd)
-
-    print('Generating PRNU and quantum efficiency CKD')
-    genPRNU(conf, nc_ckd)
-
-    print('Generating stray light CKD')
-    genStray(conf, nc_ckd)
-
-    print('Generating radiometric CKD')
-    genRadiometric(conf, nc_ckd)
-
-    print('Generating detector bad pixel mask')
-    genPixelMask(conf, nc_ckd)
-
-    nc_ckd.close()
+    gen_ckd(conf)
--- teds/l2l4/__init__.py
+++ teds/l2l4/__init__.py
@@ -2,4 +2,4 @@
 # the LICENSE file in the root directory of this project.
 '''Functions for using the L1-L2 processor.'''
 
-from teds.l2l4.l4 import level2_to_level4_processor 
+from teds.l2l4.l4 import level2_to_level4_processor  # noqa: F401

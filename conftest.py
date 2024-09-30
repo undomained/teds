9f114535a57775ccc799a105566d953120cd0663
Raul Laasner <raullaasner@gmail.com>
2024-09-30 17:53:10 +0200

    Make Pytest find TEDS

---
+++ conftest.py
@@ -0,0 +1,4 @@
+# This source code is licensed under the 3-clause BSD license found in
+# the LICENSE file in the root directory of this project.
+
+# This top-level file is required so that Pytest can find TEDS.

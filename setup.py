127ffb44a8858295f3481930706ad874542c517f
Raul Laasner <raullaasner@gmail.com>
2025-01-05 20:11:17 +0100

    Update Python build process
    * Add HDF5 library to the Docker container.
    * In setup.py, attempt to automatically detect and link to the HDF5
      library if not in standard location.
    * In bitbucket-pipelines.yml, add step to build the Python geolocation
      interface library.

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 # Instructions for Setuptools for building the Python geolocation
-# interface with a Numpy dependency.
+# interface with some library dependencies.
 
 from setuptools import Extension
 from setuptools import setup
 import numpy
+import os
+import subprocess
 
 sources = ['teds/l1al1b/tango_l1b/algorithm.cpp',
            'teds/l1al1b/tango_l1b/quaternion.cpp',
@@ -11,9 +13,31 @@ sources = ['teds/l1al1b/tango_l1b/algorithm.cpp',
            'teds/l1al1b/tango_l1b/dem.cpp',
            'teds/l1al1b/tango_l1b/geometry.cpp',
            'teds/l1al1b/python/geolocation.cpp']
+
+include_dirs = [numpy.get_include()]
+library_dirs = []
+
+# Attempt to find the HDF5 library depending on which package manager is used
+try:
+    # Determine package name and location by searching for hdf5.h
+    res = subprocess.check_output(['dpkg', '-S', 'hdf5.h']).decode('utf-8')
+    package_name, hdf5_header = res.split()
+    package_name = package_name[:-1]
+    include_dirs.append(os.path.dirname(hdf5_header))
+    # Find which directory contains the HDF5 library
+    res = subprocess.check_output(['dpkg', '-L', package_name]).decode('utf-8')
+    for line in res.split():
+        if 'libhdf5.so' in line:
+            library_dirs.append(os.path.dirname(line))
+            break
+except FileNotFoundError:
+    pass
+
 extension = Extension('teds.l1al1b.python.geolocation',
                       sources,
-                      include_dirs=[numpy.get_include()],
+                      include_dirs=include_dirs,
+                      library_dirs=library_dirs,
                       libraries=['hdf5'],
-                      extra_compile_args=['-std=c++20'])
+                      extra_compile_args=['-std=c++20'],
+                      py_limited_api=True)
 setup(ext_modules=[extension])

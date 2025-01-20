20a39160e3a985ca9c5dade5a90c585c7ac612b5
Raul Laasner <raullaasner@gmail.com>
2025-01-21 00:05:03 +0100

    Use double precision for convolution FWHM values
    Fixes #3

--- teds/lib/libNumTools.py
+++ teds/lib/libNumTools.py
@@ -188,8 +188,13 @@ def getconvolutionparams(kernel_settings, dx, dy):
         conv_settings['type'] = kernel_settings['type']
         conv_settings['1D kernel extension'] = np.int0(fsize*np.max([fwhm_x, fwhm_y])/np.min([dx, dy]))
         # convert all kernel parameter in units of sampling distance
+<<<<<<< HEAD
         conv_settings['fwhm x'] = np.int0(float(fwhm_x)/dx)
         conv_settings['fwhm y'] = np.int0(float(fwhm_y)/dy)
+=======
+        conv_settings['fwhm x'] = float(fwhm_x)/dx
+        conv_settings['fwhm y'] = float(fwhm_y)/dy
+>>>>>>> 4758b1b (Use double precision for convolution FWHM values)
     return conv_settings
 
 

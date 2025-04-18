eadcd53d28e60f0f8e71c22b8479330d5b7d3e7a
Raul Laasner <raullaasner@gmail.com>
2025-04-18 12:15:19 +0200

    Remove radiance_offset.py
    This function was not used for anything.

--- flake8_check.sh
+++ flake8_check.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-# Run flake8 to check for PEP 8 violations on all Python source files,
-# including the tests.
+# Run flake8 to check for PEP 8 violations on Python source files
+# including tests. Some source files are exluded from the check for
+# now. No new exceptions should be added.
 
-all_files=$(find teds -name "*.py" -regextype posix-extended -not -regex "teds/(lib/hapi|lib/data_netcdf/.*|lib/libAMF|lib/libDOAS|lib/libRT_no2|sgm/sgm_no2|im/create_im_configuration_file_nitro|ckd/ckd_generation/ckd_generator_nitro|ckd/ckd_generation/generator_class|ckd/ckd_generation/nitro_ckds/.*|ckd/create_binning_tables|l1al1b/create_l1a1b_configuration_file_nitro|l1l2/l1bl2_no2|siml1b/radiance_offset).py")
-flake8 $all_files
+files=$(find teds -name "*.py" -regextype posix-extended \
+             ! -wholename teds/lib/hapi.py \
+             ! -wholename teds/ckd/ckd_generation/ckd_generator_nitro.py \
+             ! -wholename teds/ckd/ckd_generation/generator_class.py \
+             ! -wholename teds/ckd/create_binning_tables.py \
+             ! -wholename "teds/ckd/ckd_generation/nitro_ckds/*" \
+             ! -wholename teds/sgm/sgm_no2.py \
+             ! -wholename teds/im/create_im_configuration_file_nitro.py \
+             ! -wholename teds/l1al1b/create_l1a1b_configuration_file_nitro.py \
+             ! -wholename teds/l1l2/l1bl2_no2.py \
+             ! -wholename teds/lib/libRT_no2.py \
+             ! -wholename teds/lib/libDOAS.py \
+             ! -wholename teds/lib/libAMF.py \
+             ! -wholename "teds/lib/data_netcdf/*" \
+         )
+flake8 $files
 exit $?
